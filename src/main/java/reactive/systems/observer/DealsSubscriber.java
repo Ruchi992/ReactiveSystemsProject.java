2ac5171dd2f109dcc88bfe0872e0fe21c91424be
Ruchi Devi <ruchidevi1987@gmail.com>
2020-08-20 17:40:13 +0100

    error

--- src/main/java/reactive/systems/observer/DealsSubscriber.java
+++ src/main/java/reactive/systems/observer/DealsSubscriber.java
@@ -1,17 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reactive.systems.observer;
 
-import reactive.systems.observer.Observer;
-
-
-/**
- *
- * @author James
- */
 public class DealsSubscriber implements Observer {
 
     String name;
@@ -19,14 +7,9 @@ public class DealsSubscriber implements Observer {
     public DealsSubscriber(String name){
         this.name =name;
     }
-
-	DealsSubscriber() {
-		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
     
     @Override
     public void update(String update) {
         System.out.println(name +" received a message: "+update);
     }
-    
 }
